703629d426a5b6bc047f48c9ea2cfe512845eb76
jhraju <jhraju890@gmail.com>
2024-03-22 00:26:30 +0600

    added nav bar and propTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-     <h1 className='text-6xl bg-red-500'>Knowledge Cafe</h1>
+    <Header></Header>
+    <div className='md:flex'>
+    <Blogs></Blogs>
+    <Bookmarks></Bookmarks>
+    </div>
     </>
   )
 }
