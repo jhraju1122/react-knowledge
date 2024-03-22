c5f6afde733b7c300a20470d910a4c4cbfb2c8a7
jhraju <jhraju890@gmail.com>
2024-03-22 17:12:12 +0600

    added propile pic

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,19 @@
 import PropTypes from 'prop-types'; 
 const Blog = ({ blog }) => {
-   const {title, cover} = blog;
+   const {title, cover, reading_time,  author , author_img } = blog;
     return (
         <div>
             <img src={cover} alt={`Cover picture of the author {title}`} />
+
+        <div className='flex justify-between'>
+            <div>
+                <img className='w-14 rounded-[50%]' src={author_img} alt="" />
+            </div>
+            <div>
+                <span>{reading_time} read</span>
+            </div>
+        </div>
+
             <h2 className="text-4xl">{title}</h2>
            
         </div>
